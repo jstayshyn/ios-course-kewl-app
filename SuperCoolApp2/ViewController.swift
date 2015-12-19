62b0decc392401ee18048c815fc62675ba933884
jstayshyn <justinstayshyn@gmail.com>
2015-12-19 14:52:08 -0500

    Initial Commit

---
+++ SuperCoolApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCoolApp2
+//
+//  Created by Justin Stayshyn on 2015-12-19.
+//  Copyright © 2015 Justin Stayshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
